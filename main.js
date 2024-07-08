4740f29b4a3d58d5cd3ddfed466f0f5159732099
Lautaro Severino <122574204+LautiSeverino@users.noreply.github.com>
2024-07-08 01:28:20 -0300

    Finish Portfolio

---
+++ main.js
@@ -0,0 +1,38 @@
+const navContenedor = document.querySelector("#hamburger-nav")
+const perfilButtons = document.querySelector(".btn-container")
+
+function returnMenu() {
+    navContenedor.innerHTML = `
+    <div class="logo"><p>Lautaro Severino</p></div>
+    <div class="hamburger-menu">
+        <div class="hamburger-icon" onclick="abrirMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="menu-links">
+            <li><a href="#sobre-mi" onclick="abrirMenu()">Sobre mi</a></li>
+            <li><a href="#conocimientos" onclick="abrirMenu()">Conocimientos</a></li>
+            <li><a href="#proyectos" onclick="abrirMenu()">Proyectos</a></li>
+            <li><a href="#contacto" onclick="abrirMenu()">Contacto</a></li>
+        </div>
+    </div>`
+}
+
+function abrirMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+
+}
+
+function abrirVentanaCV() {
+    perfilButtons.innerHTML = `
+    <button class="btn btn-color-1" onclick="window.open('./assets/Lautaro_Severino_CV.pdf')">Descargar CV</button>
+    <button class="btn btn-color-2" onclick="location.href='#contacto'">Contacto</button>`
+}
+
+abrirVentanaCV()
+returnMenu()
+
